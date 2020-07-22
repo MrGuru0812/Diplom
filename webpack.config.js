d1f280c487a32b927f2c2a7715191c79c41a9c21
Mikhail Gurtsev <gurtsevmr@yandex.ru>
2020-07-22 20:27:01 +0300

    Начало проекта, подготовка окружения

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+const path = require('path');
+// eslint-disable-next-line no-undef
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js',
+        publicPath: '/dist'
+    },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    }
+};
