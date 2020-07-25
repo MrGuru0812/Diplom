91b44e8748221cee643c7502070ccd41ab7e927a
Mikhail Gurtsev <gurtsevmr@yandex.ru>
2020-07-25 15:21:13 +0300

    скидка, аккордеон, больше

--- src/modules/moduleAddSentence.js
+++ src/modules/moduleAddSentence.js
@@ -1,4 +1,20 @@
 function moduleAddSentence() {
+    const addSentenceBtn = document.querySelector('.add-sentence-btn'),
+        sentence = document.querySelector('.sentence'),
+        row = document.querySelectorAll('.row')[4];
+    console.dir(row);
+    addSentenceBtn.addEventListener('click', () => {
 
+        const arr = [];
+        for (const i in row.children)
+            arr[i] = row.children[i];
+        arr.forEach(item => {
+            if (item.classList.contains('hidden') || item.classList.contains('visible-sm-block')) {
+                item.classList.remove('hidden');
+                item.classList.remove('visible-sm-block');
+            }
+            addSentenceBtn.classList.add('hidden');
+        });
+    });
 }
 export default moduleAddSentence;
