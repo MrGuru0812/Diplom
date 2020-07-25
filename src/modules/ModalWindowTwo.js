63da61a8b9cf441a54b9e89d95b8be293bed1d60
Mikhail Gurtsev <gurtsevmr@yandex.ru>
2020-07-25 06:27:06 +0300

    второе модальное окно

--- src/modules/ModalWindowTwo.js
+++ src/modules/ModalWindowTwo.js
@@ -1,5 +1,24 @@
 function modalWindowTwo() {
-const popupDiscount = document.querySelector('.popup-discount');
+    const popupDiscount = document.querySelector('.popup-discount'),
+        discountBtn = document.querySelectorAll('.discount-btn');
+    discountBtn.forEach(item => {
+        item.addEventListener('click', event => {
+            event.preventDefault();
+            popupDiscount.style.display = 'block';
+        });
+    });
+    popupDiscount.addEventListener('click', event => {
+        let target = event.target;
+        if (target.classList.contains('popup-close')) {
+            popupDiscount.style.display = 'none';
+        } else {
+            target = target.closest('.popup-content');
+            if (!target) {
+                popupDiscount.style.display = 'none';
+            }
+        }
+    });
+
 }
 
 export default modalWindowTwo;
